e09bd4cc6c03aef75a37654c69ef701126806869
Michael Beasley <michael.beasley@alvaria.com>
2025-08-26 13:22:41 -0800

    switch from nixpkgs-unstable to nixos-unstable-small

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Darwin system flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
