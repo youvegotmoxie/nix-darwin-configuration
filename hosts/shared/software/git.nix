d474862b2abbadfcc5e1cb62ea0902f2cdf9ab90
Michael Beasley <michael.beasley@alvaria.com>
2025-08-29 10:15:53 -0800

    undo remove with

--- hosts/shared/software/git.nix
+++ hosts/shared/software/git.nix
@@ -36,12 +36,13 @@ in {
       enable = true;
       userName = "${cfg.name}";
       userEmail = "${cfg.email}";
-      signing.key = "~/.ssh/id_rsa.pub";
+      signing.key = "${config.home.homeDirectory}/.ssh/git-signing_ed25519.pub";
       extraConfig = {
         core = {
           pager = "delta --pager=never --max-line-length=0";
           editor = "hx";
         };
+        gpg.format = "ssh";
         init = {
           defaultBranch = "master";
         };
