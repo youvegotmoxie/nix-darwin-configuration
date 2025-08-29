c4668756d8a602f7417026db293333c9e2e8fe3d
Michael Beasley <michael.beasley@alvaria.com>
2025-08-29 04:45:11 -0800

    update flake and restic launch agent schedule

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Darwin system flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
