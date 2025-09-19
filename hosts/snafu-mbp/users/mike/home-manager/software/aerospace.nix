41c4e455b499e847066dff98fcf0e7bb010b2c0c
MikeB <youvegotmoxie@gmail.com>
2025-09-18 18:30:53 -0800

    update flake

--- hosts/snafu-mbp/users/mike/home-manager/software/aerospace.nix
+++ hosts/snafu-mbp/users/mike/home-manager/software/aerospace.nix
@@ -1,3 +1,30 @@
-{pkgs, ...}: {
-  programs.aerospace.enable = true;
+{
+  programs = {
+    aerospace = {
+      enable = false;
+      userSettings = {
+        mode.main.binding = {
+          alt-h = "focus left";
+          alt-j = "focus down";
+          alt-k = "focus up";
+          alt-l = "focus right";
+          alt-slash = "layout tiles horizontal vertical";
+          alt-comma = "layout accordion horizontal vertical";
+          alt-minus = "resize smart -50";
+          alt-equal = "resize smart +50";
+          alt-1 = "workspace 1";
+          alt-2 = "workspace 2";
+          alt-3 = "workspace 3";
+          alt-shift-1 = "move-node-to-workspace 1";
+          alt-shift-2 = "move-node-to-workspace 2";
+          alt-shift-3 = "move-node-to-workspace 3";
+          alt-shift-h = "move left";
+          alt-shift-j = "move down";
+          alt-shift-k = "move up";
+          alt-shift-l = "move right";
+          alt-shift-tab = "move-workspace-to-monitor --wrap-around next";
+        };
+      };
+    };
+  };
 }
