98314dfa63ae8bf292862b57d46f9c7488daba82
Michael Beasley <michael.beasley@alvaria.com>
2025-10-09 09:16:45 -0800

    update path excludes for btop

--- hosts/mike-macbook-air/users/mike/home-manager/software/btop.nix
+++ hosts/mike-macbook-air/users/mike/home-manager/software/btop.nix
@@ -7,7 +7,7 @@
       vim_keys = true;
       update_ms = 2000;
       proc_per_core = true;
-      disks_filter = "exclude=/System/Volumes/VM /System/Volumes/Preboot /System/Volumes/Update /System/Volumes/xarts /System/Volumes/iSCPreboot /System/Volumes/Hardware /System/Volumes/Update/SFR/mnt1 /System/Volumes/Update/mnt1 /nix /Users/michaelbeasley/OrbStack, disk_filter=/dev/disk4";
+      disks_filter = "exclude=/System/Volumes/VM /System/Volumes/Preboot /System/Volumes/Update /System/Volumes/xarts /System/Volumes/iSCPreboot /System/Volumes/Hardware /System/Volumes/Update/SFR/mnt1 /System/Volumes/Update/mnt1 /nix /Users/michaelbeasley/OrbStack /Volumes/.timemachine, disk_filter=/dev/disk4";
       net_iface = "en0";
     };
     themes = {
