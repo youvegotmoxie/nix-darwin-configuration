6dee95b5ff9fa5d7b1b30be3d58456d19ba499c2
Michael Beasley <youvegotmoxie@gmail.com>
2026-02-06 19:05:51 -0900

    consolidate tmux config

--- hosts/MBeasley-23MBP.local/users/mike/home-manager/software/default.nix
+++ hosts/MBeasley-23MBP.local/users/mike/home-manager/software/default.nix
@@ -2,6 +2,5 @@
   imports = [
     ./gnupg.nix
     ./pkgs.nix
-    ./tmux.nix
   ];
 }
--- hosts/shared/software/default.nix
+++ hosts/shared/software/default.nix
@@ -3,9 +3,9 @@
     ./atuin.nix
     ./btop.nix
     ./git.nix
-    # ./helix.nix
     ./starship.nix
     ./pass.nix
+    ./tmux.nix
     ./yazi.nix
     ./zoxide.nix
     ./zsh.nix
