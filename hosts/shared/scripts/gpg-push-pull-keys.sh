9cf45bdf227be4713e691ae0d8fd24f76f74f445
Michael Beasley <youvegotmoxie@gmail.com>
2026-02-13 15:08:48 -0900

    fix bad IPv6 routing to keyservers

--- hosts/shared/scripts/gpg-push-pull-keys.sh
+++ hosts/shared/scripts/gpg-push-pull-keys.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 declare KEYID="${1}"
-declare -a keyservers=(keyserver.ubuntu.com pgp.mit.edu keys.openpgp.org)
+declare -a keyservers=(185.125.188.26 18.9.60.141 keys.openpgp.org)
 
 echo "Sending key: ${KEYID}..."
 for ks in "${keyservers[@]}"; do
-    gpg --keyserver "${ks}" --send-keys "${KEYID}"
+    gpg --verbose --keyserver "hkp://${ks}" --send-keys "${KEYID}"
 done
 
 echo "Refreshing keys..."
 for ks in "${keyservers[@]}"; do
-    gpg --keyserver "${ks}" --refresh-keys
+    gpg --verbose --keyserver "hkp://${ks}" --refresh-keys
 done
