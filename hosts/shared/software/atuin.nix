9e99d192bd89d5849322c754140348f0511bcbc2
Michael Beasley <youvegotmoxie@gmail.com>
2026-02-16 00:10:29 -0900

    change sync time for atuin

--- hosts/shared/software/atuin.nix
+++ hosts/shared/software/atuin.nix
@@ -10,7 +10,7 @@
     ];
     settings = {
       auto_sync = true;
-      sync_frequency = "10m";
+      sync_frequency = "5m";
       search_mode = "fulltext";
       filter_mode = "host";
       inline_height = 20;
