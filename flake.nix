11e9be688dd5531136404f64a151ca396900783e
Michael Beasley <youvegotmoxie@gmail.com>
2026-03-02 18:38:35 -0900

    ✨ feature: refactor flake part 2
    use mkDarwinHost function and cut down on a lot of repetition.
    
    move home comfigs around and merge some nixpkgs config
    
    Signed-off-by: Michael Beasley <youvegotmoxie@gmail.com>

--- flake.nix
+++ flake.nix
@@ -19,18 +19,21 @@
     systems,
     nil,
     ...
-  }: {
-    darwinConfigurations = {
-      "MBeasley-23MBP" = nix-darwin.lib.darwinSystem {
-        system = systems;
+  }: let
+    mkDarwinHost = {
+      mainUser,
+      hostDir,
+    }:
+      nix-darwin.lib.darwinSystem {
+        system = "aarch64-darwin";
         modules = [
-          ./hosts/MBeasley-23MBP.local/darwin.nix
+          ./hosts/${hostDir}/darwin.nix
           home-manager.darwinModules.home-manager
           {
             home-manager = {
-              users."michaelbeasley" = {
+              users.${mainUser} = {
                 imports = [
-                  ./hosts/MBeasley-23MBP.local/users/mike/home-manager/home.nix
+                  ./hosts/${hostDir}/users/${mainUser}/home-manager/home.nix
                 ];
               };
               useGlobalPkgs = true;
@@ -41,33 +44,18 @@
           }
         ];
         specialArgs = {
-          inherit inputs;
-          mainUser = "michaelbeasley";
+          inherit inputs mainUser;
         };
       };
-      "mike-macbook-air" = nix-darwin.lib.darwinSystem {
-        system = systems;
-        modules = [
-          ./hosts/mike-macbook-air/darwin.nix
-          home-manager.darwinModules.home-manager
-          {
-            home-manager = {
-              users."mike" = {
-                imports = [
-                  ./hosts/mike-macbook-air/users/mike/home-manager/home.nix
-                ];
-              };
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              backupFileExtension = "hmback";
-              extraSpecialArgs.flake-inputs = inputs;
-            };
-          }
-        ];
-        specialArgs = {
-          inherit inputs;
-          mainUser = "mike";
-        };
+  in {
+    darwinConfigurations = {
+      "MBeasley-23MBP" = mkDarwinHost {
+        mainUser = "michaelbeasley";
+        hostDir = "MBeasley-23MBP.local";
+      };
+      "mike-macbook-air" = mkDarwinHost {
+        mainUser = "mike";
+        hostDir = "mike-macbook-air";
       };
     };
   };
--- hosts/mike-macbook-air/users/mike/home-manager/home.nix
+++ hosts/mike-macbook-air/users/mike/home-manager/home.nix
@@ -32,26 +32,6 @@
     file = {
       ".zsh.d/func.zsh".source = ../../../../shared/dots/func.zsh;
       ".vimrc".source = ../../../../shared/dots/dot_vimrc;
-
-      "${config.home.homeDirectory}/Library/Application Support/com.mitchellh.ghostty/config".text = ''
-        macos-titlebar-style = tabs
-        theme = tokyonight
-        font-family = "MesloLGS NF"
-        cursor-style-blink = false
-        font-size = 13
-        cursor-style =
-        link-url = true
-        scrollback-limit = 100_000_000
-        shell-integration = detect
-        clipboard-read = allow
-        clipboard-write = allow
-        clipboard-paste-protection = true
-        mouse-hide-while-typing = true
-        confirm-close-surface = false
-        quit-after-last-window-closed = true
-        window-colorspace = display-p3
-        keybind = global:cmd+grave_accent=toggle_quick_terminal
-      '';
     };
   };
 }
