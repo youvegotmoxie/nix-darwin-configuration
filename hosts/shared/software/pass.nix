c5b2282936ac26fe9bbc4732ac24ed74caec7553
Michael Beasley <youvegotmoxie@gmail.com>
2026-03-24 06:28:30 -0800

    update flake and fix password-store env var

--- hosts/shared/software/pass.nix
+++ hosts/shared/software/pass.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   programs.password-store = {
     enable = true;
     package = pkgs.pass.withExtensions (extension: [extension.pass-checkup extension.pass-otp]);
@@ -6,6 +10,7 @@
       PASSWORD_STORE_CLIP_TIME = "60";
       PASSWORD_STORE_ENABLE_EXTENSIONS = "true";
       PASSWORD_STORE_GENERATED_LENGTH = "14";
+      PASSWORD_STORE_DIR = "${config.home.homeDirectory}/.local/share/password-store";
     };
   };
 }
