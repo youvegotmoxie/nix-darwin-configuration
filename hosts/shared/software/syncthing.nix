dac0e761dba63699e3bfa0178af1f00df0ef7aec
Michael Beasley <michael.beasley@alvaria.com>
2026-05-29 08:21:04 -0800

    reenable syncthing
    Signed-off-by: Michael Beasley <michael.beasley@alvaria.com>

--- hosts/shared/software/syncthing.nix
+++ hosts/shared/software/syncthing.nix
@@ -1,15 +1,10 @@
 {config, ...}: {
   services.syncthing = {
-    enable = false;
+    enable = true;
     overrideFolders = true;
     overrideDevices = false;
     settings = {
       folders = {
-        "${config.home.homeDirectory}/.config/zed" = {
-          id = "zed-config";
-          path = "${config.home.homeDirectory}/.config/zed";
-          label = "zed-config-directory";
-        };
         "${config.home.homeDirectory}/.agents/skills" = {
           id = "zed-skills";
           path = "${config.home.homeDirectory}/.agents/skills";
