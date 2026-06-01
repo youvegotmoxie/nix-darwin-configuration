374aec7a82c0e9eee1236533de4afa4f90a7d2c7
Michael Beasley <michael.beasley@alvaria.com>
2026-06-01 08:48:05 -0800

    revert
    Signed-off-by: Michael Beasley <michael.beasley@alvaria.com>

--- hosts/mike-macbook-pro/users/mike/home-manager/home.nix
+++ hosts/mike-macbook-pro/users/mike/home-manager/home.nix
@@ -29,6 +29,7 @@
   # See shared/software/sops.nix for Launchd configuration
   # if secrets are needed outside of the shell environment
   sops = {
+    defaultSopsFile = ./secrets/secrets.yaml;
     secrets.gh_token = {
       path = "${config.home.homeDirectory}/.creds.d/gh_token";
       mode = "0600";
