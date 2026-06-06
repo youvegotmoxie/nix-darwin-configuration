a19babb0bf2402cdeb6143326cc49ab8a9df0323
Michael Beasley <youvegotmoxie@gmail.com>
2026-06-06 13:42:32 -0800

    Revert "use rustup"
    This reverts commit 9e4dd12614509e6575e823531bb91a3963b5558e.

--- hosts/shared/software/pkgs.nix
+++ hosts/shared/software/pkgs.nix
@@ -2,6 +2,7 @@
   pkgs,
   config,
   lib,
+  flake-inputs,
   ...
 }: let
   # Package shell scripts
@@ -82,6 +83,8 @@
       mainProgram = finalAttrs.pname;
     };
   });
+
+  rust-overlay-pkgs = pkgs.extend flake-inputs.rust-overlay.overlays.default;
   cfg = config.zshConfig;
 in {
   config = {
@@ -111,7 +114,7 @@ in {
           pinentry-tty
           prek
           ripgrep
-          rustup
+          rust-overlay-pkgs.rust-bin.stable."1.95.0".default
           shfmt
           sops
           tldr
