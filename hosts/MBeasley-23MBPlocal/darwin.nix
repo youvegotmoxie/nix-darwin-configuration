0d2ebe24231d17f808d41e372e0b11420e929c9f
Michael Beasley <michael.beasley@alvaria.com>
2026-06-08 12:15:18 -0800

    🔧 chore(tools): Let nix manage ssh keys
    Use nix to manage authorized_keys
    
    Signed-off-by: Michael Beasley <michael.beasley@alvaria.com>

--- hosts/MBeasley-23MBPlocal/darwin.nix
+++ hosts/MBeasley-23MBPlocal/darwin.nix
@@ -1,5 +1,10 @@
-{...}: {
+{mainUser, ...}: {
   imports = [../shared/darwin.nix];
+
+  # Allow SSH from personal laptop
+  users.users.${mainUser}.openssh.authorizedKeys.keys = [
+    "ecdsa-sha2-nistp384 AAAAE2VjZHNhLXNoYTItbmlzdHAzODQAAAAIbmlzdHAzODQAAABhBCC3+lHWSgBAOLx2HgQLDW81dCkgAj4Jvp5bRMOl3ZlHZXbmXZwA8JYPMWiZEzOZlNXkS8UlaiC6vaq8JtPeFNziuLgQ5Ntl2AX4fk+/VpnsouQ7tvPt5wwFdiTcT811Ng== cardno:31_399_365"
+  ];
 
   # Use Homebrew for things not working with nixpkgs on macOS
   homebrew = {
--- hosts/mike-macbook-pro/darwin.nix
+++ hosts/mike-macbook-pro/darwin.nix
@@ -1,6 +1,10 @@
-{...}: {
+{mainUser, ...}: {
   imports = [../shared/darwin.nix];
 
+  # Allow SSH from work laptop
+  users.users.${mainUser}.openssh.authorizedKeys.keys = [
+    "ecdsa-sha2-nistp384 AAAAE2VjZHNhLXNoYTItbmlzdHAzODQAAAAIbmlzdHAzODQAAABhBNKNWVZe8zRvZ8VNfsDr+KQfDYvi/+ssXo6hIHLFsxwVYya+BcyFZ6TBXARrLONhkKbq4nkEA2CRatJ5bL8WG2H8dnl/WbsV+LQ5NRZz20f0MIKhOkZa6uoZE6gGWEVIxA== cardno:35_285_426"
+  ];
   # Use Homebrew for these to avoid a bunch of compiling
   homebrew = {
     brews = [
