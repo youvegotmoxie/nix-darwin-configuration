136e197d903eb494d0aaec890bdad8568811e36c
Michael Beasley <michael.beasley@alvaria.com>
2026-06-09 08:24:24 -0800

    manage raycast local LLM providers config
    Signed-off-by: Michael Beasley <michael.beasley@alvaria.com>

--- hosts/shared/software/misc-dots.nix
+++ hosts/shared/software/misc-dots.nix
@@ -3,9 +3,10 @@
     ".config/mole/whitelist".source = ../dots/mole_whitelist;
     ".config/mole/purge_paths".source = ../dots/mole_purge_paths;
     ".config/mole/whitelist_optimize".source = ../dots/mole_whitelist_optimize;
+    ".config/raycast/ai/providers.yaml".source = ../dots/raycast_providers.yaml;
+    "${config.home.homeDirectory}/Library/Application Support/com.mitchellh.ghostty/config".source = ../dots/ghostty_config;
+    ".rustup/settings.toml".source = ../dots/rustup_settings.toml;
     ".vimrc".source = ../dots/dot_vimrc;
     ".zsh.d/func.sh".source = ../dots/func.sh;
-    ".rustup/settings.toml".source = ../dots/rustup_settings.toml;
-    "${config.home.homeDirectory}/Library/Application Support/com.mitchellh.ghostty/config".source = ../dots/ghostty_config;
   };
 }
