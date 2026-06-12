7a59101b03b68294834cc87e0e1ba023ec21e19c
Michael Beasley <michael.beasley@alvaria.com>
2026-06-12 09:30:11 -0800

    update flake and clean up misc-dots.nix
    Signed-off-by: Michael Beasley <michael.beasley@alvaria.com>

--- hosts/shared/software/misc-dots.nix
+++ hosts/shared/software/misc-dots.nix
@@ -1,10 +1,10 @@
-{config, ...}: {
+{
   home.file = {
     ".config/mole/whitelist".source = ../dots/mole_whitelist;
     ".config/mole/purge_paths".source = ../dots/mole_purge_paths;
     ".config/mole/whitelist_optimize".source = ../dots/mole_whitelist_optimize;
     ".config/raycast/ai/providers.yaml".source = ../dots/raycast_providers.yaml;
-    "${config.home.homeDirectory}/Library/Application Support/com.mitchellh.ghostty/config".source = ../dots/ghostty_config;
+    "Library/Application Support/com.mitchellh.ghostty/config".source = ../dots/ghostty_config;
     ".rustup/settings.toml".source = ../dots/rustup_settings.toml;
     ".vimrc".source = ../dots/dot_vimrc;
     ".zsh.d/func.sh".source = ../dots/func.sh;
