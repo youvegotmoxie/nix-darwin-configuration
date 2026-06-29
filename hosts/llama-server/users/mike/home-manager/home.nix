5e297eb450f8e6a5da5c6a761207bf4bf2123ddc
Michael Beasley <michael.beasley@alvaria.com>
2026-06-29 11:23:54 -0800

    add config for rustup
    Signed-off-by: Michael Beasley <michael.beasley@alvaria.com>

--- hosts/llama-server/users/mike/home-manager/home.nix
+++ hosts/llama-server/users/mike/home-manager/home.nix
@@ -1,6 +1,16 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   home = {
     stateVersion = "26.05";
+    sessionVariables = rec {
+      # Needed for Zed to prevent routing loops
+      "NO_PROXY" = "localhost,127.0.0.1";
+      "no_proxy" = NO_PROXY;
+    };
+
     packages = with pkgs; [
       alejandra
       bat
@@ -24,5 +34,10 @@
       viddy
       yq
     ];
+  };
+
+  file = {
+    # Use the x86_64 Rust toolchain
+    ".rustup/settings.toml".source = lib.mkForce ./dots/rustup_settings.toml;
   };
 }
