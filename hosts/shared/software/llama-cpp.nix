6e5708b0b9c46237c732b85e7f672981753b223f
Michael Beasley <youvegotmoxie@gmail.com>
2026-06-30 21:09:26 -0800

    test minimum config
    Signed-off-by: Michael Beasley <youvegotmoxie@gmail.com>

--- hosts/shared/software/llama-cpp.nix
+++ hosts/shared/software/llama-cpp.nix
@@ -45,7 +45,7 @@ in {
         healthCheckTimeout = 60;
         models = {
           "gemma-4-e2b" = {
-            cmd = "${llama-cpp}/bin/llama-server --models-preset /var/lib/llama-cpp/models.ini --cache-ram 32400";
+            cmd = "${llama-cpp}/bin/llama-server --model /var/cache/llama-cpp/models--unsloth--gemma-4-E2B-it-qat-GGUF/gemma-4-E2B-it-qat-UD-Q4_K_XL.gguf";
             concurrencyLimit = 4;
           };
         };
@@ -69,3 +69,8 @@ in {
     };
   };
 }
+# models--unsloth--Qwen3-4B-Instruct-2507-GGUF
+# models--unsloth--gemma-4-12B-it-qat-GGUF
+# models--unsloth--gemma-4-E2B-it-qat-GGUF
+# models--unsloth--gemma-4-E4B-it-qat-GGUF
+# models--unsloth--gpt-oss-20b-GGUF
