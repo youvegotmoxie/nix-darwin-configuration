7874a14692f80ac3a7d7a68a26c8e978b4504a96
Michael Beasley <youvegotmoxie@gmail.com>
2026-07-05 07:55:55 -0800

    Disable ctrl-r keybind for fzf
    this conflicts with atuin
    
    Signed-off-by: Michael Beasley <youvegotmoxie@gmail.com>

--- hosts/shared/software/fzf.nix
+++ hosts/shared/software/fzf.nix
@@ -3,6 +3,8 @@
     enable = true;
     enableZshIntegration = true;
     defaultOptions = ["--exact"];
+    # Disable ctrl-r keybind since Atuin manages shell history
+    historyWidget.command = "";
     tmux = {
       enableShellIntegration = true;
     };
