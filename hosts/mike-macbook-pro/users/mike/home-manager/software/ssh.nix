142d0908ed8f0031a5f2d7c76fc60af5abd38608
Michael Beasley <youvegotmoxie@gmail.com>
2026-07-05 20:26:23 -0800

    simplify ssh configs
    Signed-off-by: Michael Beasley <youvegotmoxie@gmail.com>

--- hosts/mike-macbook-pro/users/mike/home-manager/software/ssh.nix
+++ hosts/mike-macbook-pro/users/mike/home-manager/software/ssh.nix
@@ -1,52 +1,46 @@
 {
   config,
-  pkgs,
   lib,
   ...
 }: {
   programs.ssh = {
-    enable = true;
-    enableDefaultConfig = false;
     includes = ["${config.home.homeDirectory}/.orbstack/ssh/config"];
-    settings = lib.mkMerge [
-      (import ../../../../../shared/software/ssh.nix {inherit config pkgs;}).programs.ssh.settings
-      {
-        "rpi4-timemachine" = {
-          Hostname = "192.168.148.217";
-          User = "mike";
-          Port = 22;
-          ControlMaster = "auto";
-          ControlPath = "${config.home.homeDirectory}/.ssh/S.%r@%h:%p";
-          ControlPersist = "2h";
-        };
-        "rpi4-standalone" = {
-          Hostname = "192.168.148.244";
-          User = "ubuntu";
-          Port = 22;
-          ControlMaster = "auto";
-          ControlPath = "${config.home.homeDirectory}/.ssh/S.%r@%h:%p";
-          ControlPersist = "2h";
-        };
-        "work-laptop" = {
-          Hostname = "192.168.148.132";
-          User = "michaelbeasley";
-          Port = 22;
-          IdentityFile = "${config.home.homeDirectory}/.ssh/id_ed25519_worklaptop_sk";
-        };
-        "mike-mac-pro" = {
-          Hostname = "192.168.148.117";
-          User = "mike";
-          Port = 22;
-        };
-        "llama-server" = {
-          Hostname = "192.168.148.125";
-          User = "mike";
-          Port = 22;
-          ControlMaster = "auto";
-          ControlPath = "${config.home.homeDirectory}/.ssh/S.%r@%h:%p";
-          ControlPersist = "2h";
-        };
-      }
-    ];
+    settings = lib.mkMerge {
+      "rpi4-timemachine" = {
+        Hostname = "192.168.148.217";
+        User = "mike";
+        Port = 22;
+        ControlMaster = "auto";
+        ControlPath = "${config.home.homeDirectory}/.ssh/S.%r@%h:%p";
+        ControlPersist = "2h";
+      };
+      "rpi4-standalone" = {
+        Hostname = "192.168.148.244";
+        User = "ubuntu";
+        Port = 22;
+        ControlMaster = "auto";
+        ControlPath = "${config.home.homeDirectory}/.ssh/S.%r@%h:%p";
+        ControlPersist = "2h";
+      };
+      "work-laptop" = {
+        Hostname = "192.168.148.132";
+        User = "michaelbeasley";
+        Port = 22;
+        IdentityFile = "${config.home.homeDirectory}/.ssh/id_ed25519_worklaptop_sk";
+      };
+      "mike-mac-pro" = {
+        Hostname = "192.168.148.117";
+        User = "mike";
+        Port = 22;
+      };
+      "llama-server" = {
+        Hostname = "192.168.148.125";
+        User = "mike";
+        Port = 22;
+        ControlMaster = "auto";
+        ControlPath = "${config.home.homeDirectory}/.ssh/S.%r@%h:%p";
+        ControlPersist = "2h";
+      };
+    };
   };
 }
--- hosts/shared/software/ssh.nix
+++ hosts/shared/software/ssh.nix
@@ -1,9 +1,29 @@
 {config, ...}: {
-  programs.ssh.settings = {
-    "*" = {
-      ForwardAgent = true;
-      StreamLocalBindUnlink = "yes";
-      IdentityAgent = "${config.home.homeDirectory}/.gnupg/S.gpg-agent.ssh";
+  programs.ssh = {
+    enable = true;
+    enableDefaultConfig = false;
+    settings = {
+      "*" = {
+        ForwardAgent = true;
+        StreamLocalBindUnlink = "yes";
+        IdentityAgent = "${config.home.homeDirectory}/.gnupg/S.gpg-agent.ssh";
+      };
+      "rpi4-timemachine" = {
+        Hostname = "192.168.148.217";
+        User = "mike";
+        Port = 22;
+        ControlMaster = "auto";
+        ControlPath = "${config.home.homeDirectory}/.ssh/S.%r@%h:%p";
+        ControlPersist = "2h";
+      };
+      "rpi4-standalone" = {
+        Hostname = "192.168.148.244";
+        User = "ubuntu";
+        Port = 22;
+        ControlMaster = "auto";
+        ControlPath = "${config.home.homeDirectory}/.ssh/S.%r@%h:%p";
+        ControlPersist = "2h";
+      };
     };
   };
 }
