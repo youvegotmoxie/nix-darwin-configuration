0a0cfd85a31a6226fc9979f6bb32c585bd1fcdb8
Michael Beasley <youvegotmoxie@gmail.com>
2026-07-05 20:39:57 -0800

    fix mkMerge in ssh.nix configs
    Signed-off-by: Michael Beasley <youvegotmoxie@gmail.com>

--- hosts/mike-mac-pro/users/mike/home-manager/software/ssh.nix
+++ hosts/mike-mac-pro/users/mike/home-manager/software/ssh.nix
@@ -5,23 +5,25 @@
 }: {
   programs.ssh = {
     includes = ["${config.home.homeDirectory}/.orbstack/ssh/config"];
-    settings = lib.mkMerge {
-      "rpi4-timemachine" = {
-        Hostname = "192.168.148.217";
-        User = "mike";
-        Port = 22;
-        ControlMaster = "auto";
-        ControlPath = "${config.home.homeDirectory}/.ssh/S.%r@%h:%p";
-        ControlPersist = "2h";
-      };
-      "rpi4-standalone" = {
-        Hostname = "192.168.148.244";
-        User = "ubuntu";
-        Port = 22;
-        ControlMaster = "auto";
-        ControlPath = "${config.home.homeDirectory}/.ssh/S.%r@%h:%p";
-        ControlPersist = "2h";
-      };
-    };
+    settings = lib.mkMerge [
+      {
+        "rpi4-timemachine" = {
+          Hostname = "192.168.148.217";
+          User = "mike";
+          Port = 22;
+          ControlMaster = "auto";
+          ControlPath = "${config.home.homeDirectory}/.ssh/S.%r@%h:%p";
+          ControlPersist = "2h";
+        };
+        "rpi4-standalone" = {
+          Hostname = "192.168.148.244";
+          User = "ubuntu";
+          Port = 22;
+          ControlMaster = "auto";
+          ControlPath = "${config.home.homeDirectory}/.ssh/S.%r@%h:%p";
+          ControlPersist = "2h";
+        };
+      }
+    ];
   };
 }
