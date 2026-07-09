ca0cef26cafd746b9791f3020b15b83601f1f90b
Michael Beasley <youvegotmoxie@gmail.com>
2026-07-08 17:09:53 -0800

    setup open-webui service
    Signed-off-by: Michael Beasley <youvegotmoxie@gmail.com>

--- hosts/shared/software/llama-cpp.nix
+++ hosts/shared/software/llama-cpp.nix
@@ -20,6 +20,15 @@
       '';
     };
   });
+  open-webui = pkgs.open-webui.overrideAttrs (oldAttrs: {
+    version = "0.10.2";
+    src = pkgs.fetchFromGitHub {
+      owner = "open-webui";
+      repo = "open-webui";
+      tag = "v${oldAttrs.version}";
+      hash = "sha256-tJ9b5up5FoX5TrmpwMWevyA/o3Ai/lKsHu+nahc2Ttc=";
+    };
+  });
   llama-cpp =
     (pkgs.llama-cpp.override {
       rocmSupport = true;
@@ -61,6 +70,12 @@ in {
   };
   users.groups.llama-swap = {};
   services = {
+    open-webui = {
+      enable = true;
+      package = open-webui;
+      port = 3000;
+      host = "0.0.0.0";
+    };
     llama-swap = {
       enable = false;
       package = llama-swap;
