e58c86a66b8b7704c03700a4b8d9601e479118c3
Michael Beasley <youvegotmoxie@gmail.com>
2026-07-09 16:58:14 -0800

    split llama-cpp and llama-swap modules
    Signed-off-by: Michael Beasley <youvegotmoxie@gmail.com>

---
+++ hosts/shared/software/llama-cpp.nix
@@ -0,0 +1,54 @@
+{ pkgs, ... }: let
+  llama-cpp =
+    (pkgs.llama-cpp.override {
+      rocmSupport = true;
+      blasSupport = true;
+    }).overrideAttrs
+    (oldAttrs: {
+      version = "9946";
+      src = pkgs.fetchFromGitHub {
+        owner = "ggml-org";
+        repo = "llama.cpp";
+        tag = "b${oldAttrs.version}";
+        hash = "sha256-wtaHsVOyCNCITABe1TvDo/MiWpNlH2YqZewBDxERtt4=";
+        leaveDotGit = true;
+        postFetch = ''
+          git -C "$out" rev-parse --short HEAD > $out/COMMIT
+          find "$out" -name .git -print0 | xargs -0 rm -rf
+        '';
+      };
+      npmDepsHash = "sha256-X1DZgmhS/zHTqDT5zq0kywwntthcJ9vRXeqyO3zz6UU=";
+      cmakeFlags =
+        (oldAttrs.cmakeFlags or [])
+        ++ [
+          "-DGGML_NATIVE=ON"
+          "-DGGML_HIP=ON"
+          "-DGPU_TARGETS=gfx1201"
+          "-DCMAKE_BUILD_TYPE=Release"
+        ];
+      preConfigure = ''
+        export NIX_ENFORCE_NO_NATIVE=0
+        ${oldAttrs.preConfigure or ""}
+      '';
+    });
+in {
+  services = {
+    llama-cpp = {
+      enable = true;
+      package = llama-cpp;
+      settings = {
+        models-preset = "/var/lib/llama-cpp/models.ini";
+        host = "0.0.0.0";
+        port = 8080;
+        cache-ram = 32400;
+        cache-type-k = "q8_0";
+        cache-type-v = "q8_0";
+        threads = 16;
+        kv-unified = true;
+        jinja = true;
+        prio = 2;
+      };
+    };
+  };
+  environment.systemPackages = [llama-cpp];
+}
