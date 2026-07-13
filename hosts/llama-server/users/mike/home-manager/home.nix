e9b1ab2410bdd2a43ab0b48cd63cffc7869e4558
Michael Beasley <michael.beasley@alvaria.com>
2026-07-13 11:39:18 -0800

    setup eza
    Signed-off-by: Michael Beasley <michael.beasley@alvaria.com>

--- hosts/llama-server/users/mike/home-manager/home.nix
+++ hosts/llama-server/users/mike/home-manager/home.nix
@@ -83,4 +83,36 @@
       ".zsh.d/func.sh".source = ../../../../shared/dots/func.sh;
     };
   };
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    vimAlias = true;
+    # Disable the embedded ruby and python3 interpreters
+    withRuby = false;
+    withPython3 = false;
+    # Don't overwrite ~/.config/nvim/init.lua
+    sideloadInitLua = true;
+  };
+
+  programs.direnv = {
+    enable = true;
+    config = {
+      warn_timeout = "10m";
+    };
+    nix-direnv = {
+      enable = true;
+    };
+    enableZshIntegration = true;
+  };
+
+  programs.eza = {
+    enable = true;
+    enableZshIntegration = true;
+    git = true;
+    extraOptions = [
+      "-lahg"
+      "--git-repos-no-status"
+    ];
+  };
 }
