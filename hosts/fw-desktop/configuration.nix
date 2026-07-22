3ed9bdc9285db37100a2f89cd65999fde535f76e
Michael Beasley <youvegotmoxie@gmail.com>
2026-07-21 18:41:02 -0800

    add llama-server ssh key for rsync
    Signed-off-by: Michael Beasley <youvegotmoxie@gmail.com>

--- hosts/fw-desktop/configuration.nix
+++ hosts/fw-desktop/configuration.nix
@@ -48,6 +48,7 @@
     openssh.authorizedKeys.keys = [
       "ecdsa-sha2-nistp384 AAAAE2VjZHNhLXNoYTItbmlzdHAzODQAAAAIbmlzdHAzODQAAABhBNKNWVZe8zRvZ8VNfsDr+KQfDYvi/+ssXo6hIHLFsxwVYya+BcyFZ6TBXARrLONhkKbq4nkEA2CRatJ5bL8WG2H8dnl/WbsV+LQ5NRZz20f0MIKhOkZa6uoZE6gGWEVIxA== cardno:35_285_426"
       "ecdsa-sha2-nistp384 AAAAE2VjZHNhLXNoYTItbmlzdHAzODQAAAAIbmlzdHAzODQAAABhBCC3+lHWSgBAOLx2HgQLDW81dCkgAj4Jvp5bRMOl3ZlHZXbmXZwA8JYPMWiZEzOZlNXkS8UlaiC6vaq8JtPeFNziuLgQ5Ntl2AX4fk+/VpnsouQ7tvPt5wwFdiTcT811Ng== cardno:31_399_365"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFmaOAZ3pmnarfX315CnWGxpLVhoQpoENZHo2uIoKYQO mike@llama-server"
     ];
     packages = with pkgs; [neovim];
   };
