3ee6574fd82ab30a5b086d880a0732fc649976b8
Michael Beasley <youvegotmoxie@gmail.com>
2026-07-22 19:22:02 -0800

    remove it this time?
    Signed-off-by: Michael Beasley <youvegotmoxie@gmail.com>

--- hosts/llama-server/configuration.nix
+++ hosts/llama-server/configuration.nix
@@ -5,7 +5,7 @@
   ];
 
   services.llama-cpp.enable = lib.mkForce false;
-  systemd.services.llama-cpp.serviceConfig = lib.mkForce {};
+  systemd.services.llama-cpp.enable = lib.mkForce false;
 
   users.users.${mainUser}.openssh.authorizedKeys.keys = [
     "ecdsa-sha2-nistp384 AAAAE2VjZHNhLXNoYTItbmlzdHAzODQAAAAIbmlzdHAzODQAAABhBNKNWVZe8zRvZ8VNfsDr+KQfDYvi/+ssXo6hIHLFsxwVYya+BcyFZ6TBXARrLONhkKbq4nkEA2CRatJ5bL8WG2H8dnl/WbsV+LQ5NRZz20f0MIKhOkZa6uoZE6gGWEVIxA== cardno:35_285_426"
