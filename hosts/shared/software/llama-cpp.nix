491b79eb579167d96b7627a4677189516e8355bf
Michael Beasley <michael.beasley@alvaria.com>
2026-07-29 10:29:57 -0800

    bump llama-cpp and add laguna model
    Signed-off-by: Michael Beasley <michael.beasley@alvaria.com>

--- hosts/shared/software/llama-cpp.nix
+++ hosts/shared/software/llama-cpp.nix
@@ -5,7 +5,7 @@
       blasSupport = true;
     }).overrideAttrs
     (oldAttrs: {
-      version = "10167";
+      version = "10182";
       src = pkgs.fetchFromGitHub {
         owner = "ggml-org";
         repo = "llama.cpp";
