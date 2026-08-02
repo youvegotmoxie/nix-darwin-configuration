b13baa26c005fa7e403d1da8e95b2a87d0e8a7a2
Michael Beasley <youvegotmoxie@gmail.com>
2026-08-01 23:30:17 -0800

    remove duped defaults
    Signed-off-by: Michael Beasley <youvegotmoxie@gmail.com>

--- hosts/mike-macbook-pro/users/mike/home-manager/home.nix
+++ hosts/mike-macbook-pro/users/mike/home-manager/home.nix
@@ -6,12 +6,6 @@
     ./software
   ];
 
-  # Configure gpg-agent
-  gpgConfig = {
-    pubKey = "26693209BA633C80";
-    sshKeys = ["FA2DB0DD531C864082BD10F5C936E7BFD93BA80A"];
-  };
-
   # Configure SSH agent socket
   zshConfig = {
     ssh.socketPath = "${config.home.homeDirectory}/.gnupg/S.gpg-agent.ssh";
