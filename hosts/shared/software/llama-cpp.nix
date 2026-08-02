eab5b135536bf9e967514e4a6db9148fbd74e120
Michael Beasley <youvegotmoxie@gmail.com>
2026-08-01 17:14:16 -0800

    update flake
    Signed-off-by: Michael Beasley <youvegotmoxie@gmail.com>

--- hosts/shared/software/llama-cpp.nix
+++ hosts/shared/software/llama-cpp.nix
@@ -5,7 +5,7 @@
       blasSupport = true;
     }).overrideAttrs
     (oldAttrs: {
-      version = "10199";
+      version = "10223";
       src = pkgs.fetchFromGitHub {
         owner = "ggml-org";
         repo = "llama.cpp";
