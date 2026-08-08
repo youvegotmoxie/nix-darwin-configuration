33e506eca494f732b219c44fcb27d198c4498bdd
Michael Beasley <youvegotmoxie@gmail.com>
2026-08-07 22:23:04 -0800

    change ssh hosts
    Signed-off-by: Michael Beasley <youvegotmoxie@gmail.com>

--- hosts/mike-mac-mini/users/mike/home-manager/software/ssh.nix
+++ hosts/mike-mac-mini/users/mike/home-manager/software/ssh.nix
@@ -1,34 +1,8 @@
 {
   config,
-  lib,
   ...
 }: {
   programs.ssh = {
     includes = ["${config.home.homeDirectory}/.orbstack/ssh/config"];
-    settings = lib.mkMerge [
-      {
-        "mike-mac-pro" = {
-          Hostname = "192.168.148.232";
-          User = "mike";
-          Port = 22;
-        };
-        "llama-server" = {
-          Hostname = "192.168.148.125";
-          User = "mike";
-          Port = 22;
-          ControlMaster = "auto";
-          ControlPath = "${config.home.homeDirectory}/.ssh/S.%r@%h:%p";
-          ControlPersist = "2h";
-        };
-        "fw-desktop" = {
-          Hostname = "192.168.148.148";
-          User = "mike";
-          Port = 22;
-          ControlMaster = "auto";
-          ControlPath = "${config.home.homeDirectory}/.ssh/S.%r@%h:%p";
-          ControlPersist = "2h";
-        };
-      }
-    ];
   };
 }
