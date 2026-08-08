8b96cf999aaba9cafb855dc78a76bf367f2465b8
Michael Beasley <youvegotmoxie@gmail.com>
2026-08-07 20:13:32 -0800

    add mac mini
    Signed-off-by: Michael Beasley <youvegotmoxie@gmail.com>

---
+++ hosts/mike-mac-mini/darwin.nix
@@ -0,0 +1,16 @@
+{mainUser, ...}: {
+  imports = [../shared/darwin.nix];
+
+  # Allow SSH from work and personal laptop
+  users.users.${mainUser}.openssh.authorizedKeys.keys = [
+    "ecdsa-sha2-nistp384 AAAAE2VjZHNhLXNoYTItbmlzdHAzODQAAAAIbmlzdHAzODQAAABhBNKNWVZe8zRvZ8VNfsDr+KQfDYvi/+ssXo6hIHLFsxwVYya+BcyFZ6TBXARrLONhkKbq4nkEA2CRatJ5bL8WG2H8dnl/WbsV+LQ5NRZz20f0MIKhOkZa6uoZE6gGWEVIxA== cardno:35_285_426"
+    "ecdsa-sha2-nistp384 AAAAE2VjZHNhLXNoYTItbmlzdHAzODQAAAAIbmlzdHAzODQAAABhBCC3+lHWSgBAOLx2HgQLDW81dCkgAj4Jvp5bRMOl3ZlHZXbmXZwA8JYPMWiZEzOZlNXkS8UlaiC6vaq8JtPeFNziuLgQ5Ntl2AX4fk+/VpnsouQ7tvPt5wwFdiTcT811Ng== cardno:31_399_365"
+  ];
+  # Use Homebrew for these to avoid a bunch of compiling
+  homebrew = {
+    brews = [
+      "ffmpeg"
+      "yt-dlp"
+    ];
+  };
+}
---
+++ hosts/mike-mac-mini/users/mike/home-manager/home.nix
@@ -0,0 +1,43 @@
+{config, ...}: {
+  imports = [
+    # shared modules in root of hosts dir
+    ../../../../shared/software
+    # Per host modules
+    ./software
+  ];
+
+  # Configure SSH agent socket
+  zshConfig = {
+    ssh.socketPath = "${config.home.homeDirectory}/.gnupg/S.gpg-agent.ssh";
+  };
+
+  # See shared/software/sops.nix for Launchd configuration
+  # if secrets are needed outside of the shell environment
+  sops = {
+    defaultSopsFile = ./secrets/secrets.yaml;
+    secrets.gh_token = {
+      path = "${config.home.homeDirectory}/.creds.d/gh_token";
+      mode = "0600";
+    };
+  };
+
+  home = {
+    stateVersion = "25.05";
+    sessionVariables = rec {
+      # Needed for Zed to prevent routing loops
+      "NO_PROXY" = "localhost,127.0.0.1,192.168.148.125";
+      "no_proxy" = NO_PROXY;
+    };
+
+    file = {
+      ".config/yt-dlp/config".text = ''
+        --format best[height<=1080]
+        --format-sort +size
+        --merge-output-format mp4
+        --embed-thumbnail
+        --embed-metadata
+        --output "%(title)s.%(ext)s"
+      '';
+    };
+  };
+}
