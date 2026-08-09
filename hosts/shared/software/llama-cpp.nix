3b9b31b2c18739d0aac5e107a06838cc73fdb692
Michael Beasley <youvegotmoxie@gmail.com>
2026-08-09 09:18:49 -0800

    increase parallell llama-cpp 5 -> 10
    Signed-off-by: Michael Beasley <youvegotmoxie@gmail.com>

--- hosts/shared/software/llama-cpp.nix
+++ hosts/shared/software/llama-cpp.nix
@@ -43,7 +43,7 @@ in {
         cache-type-k = "q8_0";
         cache-type-v = "q8_0";
         threads = 32;
-        parallel = 5;
+        parallel = 10;
         cont-batching = true;
         kv-unified = true;
         jinja = true;
