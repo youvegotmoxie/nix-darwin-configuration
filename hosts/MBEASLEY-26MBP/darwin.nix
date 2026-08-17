f40867447ab060cee136ae6e2506b1816865f059
Michael Beasley <michael.beasley@alvaria.com>
2026-08-17 07:08:52 -0800

    add gpu memory for work laptop

--- hosts/MBEASLEY-26MBP/darwin.nix
+++ hosts/MBEASLEY-26MBP/darwin.nix
@@ -1,6 +1,9 @@
 {mainUser, pkgs, ...}: {
   imports = [../shared/darwin.nix];
 
+  # Increase GPU memory for oMLX
+  extras.gpuMemory = "45056";
+
   # Allow SSH from personal laptop
   users.users.${mainUser}.openssh.authorizedKeys.keys = [
     "ecdsa-sha2-nistp384 AAAAE2VjZHNhLXNoYTItbmlzdHAzODQAAAAIbmlzdHAzODQAAABhBCC3+lHWSgBAOLx2HgQLDW81dCkgAj4Jvp5bRMOl3ZlHZXbmXZwA8JYPMWiZEzOZlNXkS8UlaiC6vaq8JtPeFNziuLgQ5Ntl2AX4fk+/VpnsouQ7tvPt5wwFdiTcT811Ng== cardno:31_399_365"
